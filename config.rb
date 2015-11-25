12158d15e4535125616438d7b6324b26a98ade4c
Zoltan <zdebre@gmail.com>
2015-11-25 20:07:55 +1300

    More source

---
+++ config.rb
@@ -0,0 +1,85 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+activate :syntax, :line_numbers => true
+
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
+activate :blog do |blog|
+  # set options on blog
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = "blog"
+
+  blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  blog.default_extension = ".md"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+
+
+end
+
+page "/feed.xml", layout: false
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  activate :asset_hash
+end
+
+# Pretty urls without .html files only directories.
+activate :directory_indexes
---
+++ source/2012-01-01-example-article.html.markdown
@@ -0,0 +1,18 @@
+---
+title: Example Article
+date: 2012-01-01
+tags: example
+---
+# Title
+
+## Subtitle
+
+This is an example article. You probably want to delete it and write your own articles!
+
+``` javascript
+var codesnipet = 'something';
+
+function() {
+  return codesnipet;
+}
+```
